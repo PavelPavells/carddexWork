d9ac26a26c4df9962da5a603f7154feecd64bfc4
exegate <der.shoxed@gmail.com>
2019-09-06 12:19:34 +0300

    Initial commit: Add First variant front auth; Commiter: Pavel Smirnov, 12:20

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from "./Header/Header";
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="App-header">
+        <Header />
+      </header>
+      <main>
+        <section>
+        
+        </section>
+        <section>
+
+        </section>
+      </main>
+      <footer>
+       
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import propTypes from "prop-types";
+import "../Stylesheets/Header.scss"
+
+const Header = () => (
+    <div className="header-logo">
+        <img src="../Picture/fox-logo.png" alt="header-logo"></img>
+    </div>
+)
+export default Header;
